6d02c53cdb1fd6b39c698e43e04a813eeb1726f9
Kondagoni-Sushanth <learningl764@gmail.com>
2025-03-15 17:48:26 +0000

    Datatypes_Conversion

---
+++ 01_basics/data_types.js
@@ -0,0 +1,14 @@
+//Data Types and Type conversion.
+
+console.log(typeof 12);
+console.log(typeof(12));
+let number="1234";
+let num2=1234;
+let Name="sushanth";
+let c=false;
+let nickname;
+console.table([number,Name,c,nickname]);
+let TypoNumb=Number(number);
+let TypoBool=Boolean(Name);
+let TypoString=String(num2);
+console.table([TypoNumb,TypoBool,TypoString]);
