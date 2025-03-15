44aef13a1e2d0cac9ef3bcc244c6bf6ab6f9c83c
Kondagoni-Sushanth <learningl764@gmail.com>
2025-03-15 19:05:17 +0000

    Functions

---
+++ 01_basics/Function.js
@@ -0,0 +1,29 @@
+//=== && ==
+/*console.log(5 === 5);      // true (Same type and value)
+console.log(5 === "5");    // false (Different type: number vs string)
+console.log(true === 1);   // false (Boolean vs Number)
+console.log(null === undefined); // false (Different types)
+
+console.log(5 == "5");    // true (String "5" -> Number 5)
+console.log(true == 1);   // true (true -> 1)
+console.log(null == undefined); // true (Special rule)
+console.log(" " == 0);    // true (Empty string -> 0)
+
+
+console.log(null == undefined); // true 
+//but null is > 0 but undefined does not >|<|= to any thing
+*/
+
+ const add=function sushanth(x, y) {
+    return x + y;
+};
+console.log(add(5, 3)); // Output: 8
+
+//orelse
+function add1(x,y){
+return x+y;
+}
+console.log(add1(5, 3)); // Output: 8
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
